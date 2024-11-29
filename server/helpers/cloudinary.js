fe6e854c47949281fe537591eae5cbad979435f5
sujoyghoshal <sujoyghoshal.s@gmail.com>
2024-11-29 12:22:29 +0530

    adding github this client and servr

---
+++ server/helpers/cloudinary.js
@@ -0,0 +1,27 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+const dotenv = require("dotenv");
+dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
+  api_key: process.env.CLOUDINARY_API_KEY || "",
+  api_secret: process.env.CLOUDINARY_API_SECRET || "",
+});
+
+const storage = multer.memoryStorage();
+
+async function imageUploadUtil(file) {
+  try {
+    const result = await cloudinary.uploader.upload(file, {
+      resource_type: "auto", 
+    });
+    return result;
+  } catch (error) {
+    console.error("Error uploading to Cloudinary:", error);
+    throw error;
+  }
+}
+
+const upload = multer({ storage });
+
+module.exports = { upload, imageUploadUtil };
---
+++ server/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
+
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
+
+const commonFeatureRouter = require("./routes/common/feature-routes");
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+
+app.get('/',(req,res)=>{
+  res.json({message:'server is running'})
+})
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/admin/orders", adminOrderRouter);
+
+app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
+
+app.use("/api/common/feature", commonFeatureRouter);
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
